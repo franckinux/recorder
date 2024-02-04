d8f4672c0e485184f0da74b1027021f30a6bbd93
Franck Barbenoire <contact@franck-barbenoire.fr>
2024-02-04 20:27:18 +0100

    upgrade python packages

--- tools/check_requirements_files.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-requirements_installed=$(mktemp /tmp/requirements.XXXXXX)
-pip freeze  | LC_COLLATE=C sort > $requirements_installed
-requirements_files=$(mktemp /tmp/requirements.XXXXXX)
-cat requirements/requirements.*.txt | LC_COLLATE=C sort | uniq > $requirements_files
-diff $requirements_installed $requirements_files
--- tools/pip-upgrade-packages.sh
+++ tools/pip-upgrade-packages.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
+set -e
+
+if [[ -z $1 ]]; then
+    RANGE="local"
+elif [[ $# -eq 1 ]]; then
+    if [[ $1 == "--local" || $1 == "-l" ]]; then
+        RANGE="local"
+    elif [[ $1 == "--user" || $1 == "-u" ]]; then
+        RANGE="user"
+    else
+        echo "invalid option"
+        exit 1
+    fi
+else
+	 echo "invalid option number"
+	 exit 1
+fi
+
+echo "pip list --$RANGE --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U"
